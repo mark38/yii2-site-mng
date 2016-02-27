17a62842dd4dba0a729d951711023c027d27fd11
Timur <web.develop@mail.ru>
2016-02-27 03:55:54 +0300

    Dev shop

--- backend/modules/shop/controllers/DefaultController.php
+++ backend/modules/shop/controllers/DefaultController.php
@@ -35,7 +35,7 @@ class DefaultController extends Controller
                 echo "success\nHello1C".Yii::$app->request->cookies['Hello1C'];
             }
         } elseif (Yii::$app->request->get('type') == 'catalog' && Yii::$app->request->get('mode') == 'init') {
-            echo "zip=no\nfile_limit=3145728000";
+            echo "zip=yes\nfile_limit=314572800";
             fwrite($upload_log, '3. Определение file_limit'."\n");
         } elseif (Yii::$app->request->get('type') == 'catalog' && Yii::$app->request->get('filename')) {
             fwrite($upload_log, '4. Начало загрузки файлов'."\n");
@@ -62,6 +62,7 @@ class DefaultController extends Controller
             }
         }
 
+        fwrite($upload_log, 'Close log file'."\n");
         fclose($upload_log);
 
         return false;
