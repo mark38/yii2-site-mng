ee15813542d79c6c3bee142ff6b47a1ffc70609f
Timur Markov <markovtimur@gmail.com>
2016-05-12 22:43:15 +0300

    Config params

--- common/config/params.php
+++ common/config/params.php
@@ -1,10 +1,27 @@
 <?php
 return [
-    'adminEmail' => 'web.develop@mail.ru',
-    'supportEmail' => 'web.develop@mail.ru',
+    'adminEmail' => 'email@site.ru',
+    'supportEmail' => 'email@site.ru',
     'user.passwordResetTokenExpire' => 3600,
-    'domainName' => 'world-door.ru',
-    'phpAuthUser' => 'admin', //Логин для авторизации 1с
-    'phpAuthPw' => 'kjUd76Rt', //Пароль для авторизации 1с
-    'defaultShGlType' => 'sh', //Основная галерея для товара
+    'domainName' => 'site.ru',
+    'shop' => [
+        'phpAuthUser' => 'admin',
+        'phpAuthPw' => 'sdDcgv87N',
+        'upload_dir' => '/web/upload/shop',
+        'start_group_path' => '/КоммерческаяИнформация/Классификатор/Группы/Группа/Группы/Группа',
+        'categories_id' => 3,
+        'group_layouts_id' => 1,
+        'good_layouts_id' => 1,
+        'goods_views_id' => 2,
+        'good_views_id' => 3,
+        'gallery' => [
+            'good' => 4,
+            'group' => 9,
+            'color' => 5,
+            'Pogonazh' => 6,
+            'other' => 7,
+            'logo' => 8
+        ],
+        'gallery_link' => [4, 9],
+    ]
 ];
