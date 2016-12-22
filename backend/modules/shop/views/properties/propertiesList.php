b283b4a260f06fb62e2ba604c297c14b211bdcbf
Timur Markov <markovtimur@gmail.com>
2016-12-22 16:43:41 +0300

    Find problem

--- backend/modules/shop/views/properties/propertiesList.php
+++ backend/modules/shop/views/properties/propertiesList.php
@@ -20,8 +20,8 @@ echo Html::endTag('thead');
 
 foreach ($properties as $num => $property) {
     echo Html::tag('tr',
-        Html::tag('td', Html::tag(($property->state == 1 ? 'span' : 'del'), ($num+1))),
-        //Html::tag('td', Html::tag(($property->state == 1 ? 'span' : 'del'), ($property->name))),
+        Html::tag('td', Html::tag(($property->state == 1 ? 'span' : 'del'), ($num+1))) .
+        Html::tag('td', Html::tag(($property->state == 1 ? 'span' : 'del'), ($property->name))),
         /*Html::tag('td', Html::tag(($property->state == 1 ? 'span' : 'del'), ($property->anchor))) .
         Html::tag('td', Html::tag(($property->state == 1 ? 'span' : 'del'), ($property->url))) .
         Html::tag('td', ButtonDropdown::widget([
