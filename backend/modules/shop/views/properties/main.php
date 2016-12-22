f635ec0ad6919a4c310fc9de2cae91553da08dfc
Timur Markov <markovtimur@gmail.com>
2016-12-22 16:27:24 +0300

    Find problem

--- backend/modules/shop/views/properties/main.php
+++ backend/modules/shop/views/properties/main.php
@@ -15,39 +15,3 @@ $this->title = 'Управление свойствами и их значени
 
 ?>
 
-<div class="row">
-    <div class="col-md-6">
-        <?php if ($property) echo $this->render('propertyMng', ['property' => $property])?>
-
-        <div class="box box-default">
-            <div class="box-header with-border">
-                <h3 class="box-title">
-                    Перечень свойств
-                    <?=ButtonDropdown::widget([
-                        'label' => '<i class="glyphicon glyphicon-option-vertical"></i>',
-                        'dropdown' => [
-                            'items' => [
-                                ['label' => 'Добавить свойство', 'url' => Url::to(['action' => 'property_add'])],
-                            ],
-                        ],
-                        'encodeLabel' => false,
-                        'options' => [
-                            'class' => 'btn btn-link btn-xs root-action clear-caret',
-                        ]
-                    ])?>
-                </h3>
-            </div>
-            <div class="box-body">
-                <?php if ($properties) {
-//                    echo $this->render('propertiesList', ['properties' => $properties]);
-                }?>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-6">
-        <?php if ($value) echo $this->render('valueMng', ['value' => $value, 'content' => $content])?>
-
-        <?php if ($values) echo $this->render('valuesList', ['values' => $values]) ?>
-    </div>
-</div>
