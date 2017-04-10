121190aeffff8d21b2aa863d43d3657d83b56596
Евгений Лобанов < >
2017-04-10 23:16:46 +0300

    Edit mnguser

--- backend/modules/settings/controllers/DefaultController.php
+++ backend/modules/settings/controllers/DefaultController.php
@@ -2,19 +2,48 @@
 
 namespace app\modules\settings\controllers;
 
+use common\models\rbac\AuthItem;
+use common\models\User;
+use yii\data\ActiveDataProvider;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
+use Yii;
 
 /**
  * Default controller for the `settings` module
  */
 class DefaultController extends Controller
 {
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
-    public function actionIndex()
+    public function actionIndex($id = null, $new = null)
     {
-        return $this->render('index');
+        $dataProvider = new ActiveDataProvider([
+            'query' => User::find(),
+            'pagination' => [
+                'pageSize' => 20,
+            ],
+        ]);
+
+        $roles = ArrayHelper::map(AuthItem::find()->all(), 'name', 'name');
+
+        if ($new) {
+            $user = new User();
+        } elseif ($id) {
+            $user = User::find()->where(['id' => $id])->one();
+        }
+
+        if (isset($user) && $user->load(Yii::$app->request->post()) && $user->save()) {
+            if ($id) {
+                Yii::$app->getSession()->setFlash('success', 'Информация о пользователе обновлена.');
+            } else {
+                Yii::$app->getSession()->setFlash('success', 'Пользователь добавлен в систему');
+            }
+        } else {
+            return $this->render('index', [
+                'dataProvider' => $dataProvider,
+                'user' => isset($user) ? $user : '',
+                'roles' => $roles
+            ]);
+        }
+        return $this->redirect('/settings');
     }
 }
